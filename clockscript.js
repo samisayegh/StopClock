3e73046537afb7dc458ef115d3b8f1c03da56757
Sami Sayegh <samisayegh1@gmail.com>
2014-12-13 20:31:58 -0500

    javascript check it

---
+++ clockscript.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	var start = new Date().getTime(),
+    elapsed = '0.0';
+    
+    //function pad makes it so that a number has a 0 before it if it's a single digit.
+    function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+    }
+    
+    window.setInterval(function(){
+        var time = new Date().getTime() - start;
+
+        elapsed = Math.floor(time / 1000) ;
+        /*if(Math.round(elapsed) == elapsed) { 
+            elapsed += '.0'; 
+        }*/
+
+        var t = 10800-elapsed;
+        var hours = Math.floor(
+            t/3600
+            );
+        var minutes = Math.floor(
+            (t%3600)/60
+            );
+        var seconds = Math.floor(
+            (t%3600)%60
+            );
+            
+        /*if (seconds % 2 == 0) {
+                $('.blinker').css("visibility", "visible");
+                console.log ('tick');
+            }
+        else {
+                $('.blinker').css("visibility", "hidden");
+                console.log ('tock');
+            }*/
+        
+        $('#timer').html(hours + '<span class = "blinky"> : </span>' + pad(minutes) + '<span class = "blinky"> : </span>' + pad(seconds));
+        /*$('#hour').html(hours);
+        $('#minute').html(pad(minutes));
+        $('#second').html(pad(seconds));*/
+
+        if (seconds % 2 == 0) {
+                $('.blinky').css("visibility", "visible");
+                console.log ('tick');
+            }
+        else {
+                $('.blinky').css("visibility", "hidden");
+                console.log ('tock');
+            }
+        
+        console.log('success');
+        console.log(time);
+
+    }, 100);
+});
